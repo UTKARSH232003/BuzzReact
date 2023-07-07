3c60d37a4e75dd77560b4b8f45b2e878871e35ac
Utkarsh <utkarshsingh6207603306@gmail.com>
2023-07-07 15:56:29 +0530

    changed the navbar and spinner to functional compo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 
-export default class Navbar extends Component {
-  render() {
+const Navbar = () =>{
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <Link className="navbar-brand" to="/">Navbar</Link>
@@ -25,5 +25,5 @@ export default class Navbar extends Component {
             </div>
         </nav>
     )
-  }
 }
+export default Navbar;
